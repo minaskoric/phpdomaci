aa8e88b0ea0eb5ba71d2a9635cdb37406b2bf8dc
minaskoric <mina.skoric99@gmail.com>
2023-04-18 22:02:13 +0200

    added changes

--- konekcija.php
+++ konekcija.php
@@ -1,4 +1,3 @@
 <?php
-/* Ovde se konektujemo na bazu, i kada god includujemo konekciju mi se fakticki konektujemo na nasu bazu*/
 $konekcija = new Mysqli('localhost','root','','srbijanocu');
 $konekcija->set_charset('utf8');
